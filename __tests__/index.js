b3815ab7f9a7f5d0d7d8fbff9e2f660643dd9cad
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-07-05 21:37:55 +0530

    object

--- __tests__/index.js
+++ __tests__/index.js
@@ -5,8 +5,21 @@ describe("test case inside __test__ for sum",function(){
         expect(sum).toBeDefined()
       })
 })
-test('toBe',()=>{
+describe("toBe test",()=>{
+test('toBe Number',()=>{
     expect(10).toBe(10)
+
+})
+
+test('toBe Object a===a',()=>{
+     var a={}
+    expect(a).toBe(a)
+})
+
+
+test('toBe boolean',()=>{
+    
     expect(false).toBe(false)
-    expect({}).toBe({})
+})
+
 })
